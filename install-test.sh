5103ec78dcb64aa7eb8bed7687489c65182b14a9
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2018-06-03 07:59:53 -0500

    Remove sudo calls

--- install-test.sh
+++ install-test.sh
@@ -1,10 +1,10 @@
 main() {
 
   echo 'Installing pacman packages'
-  sudo pacman -S --noconfirm anki chromium calibre diff-so-fancy firefox git pamac pinta the_silver_searcher tmux vim wavemon yaourt zeal
+  pacman -S --noconfirm anki chromium calibre diff-so-fancy firefox git pamac pinta the_silver_searcher tmux vim wavemon yaourt zeal
 
   echo 'Installing yaourt packages'
-  sudo yaourt -S --noconfirm franz-bin heroku-cli upwork postman-bin peek rcm green-recorder
+  yaourt -S --noconfirm franz-bin heroku-cli upwork postman-bin peek rcm green-recorder
 
   #.dotfiles
   git clone https://github.com/sanchez2210/.dotfiles ~/.dotfiles
