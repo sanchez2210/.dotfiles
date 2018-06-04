393caab290a0a3ee0b1ef3189fa3308984ba24bf
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2018-06-04 13:06:05 -0500

    Fix --delete-excluded command

--- backup.sh
+++ backup.sh
@@ -1,1 +1,1 @@
-rsync -av --delete --delete-excluded $HOME/* /run/media/luis/Backup/luis/ --include-from=$HOME/.home-backup-include.txt
+rsync -av --delete $HOME/ /run/media/luis/Backup/luis/ --include-from=$HOME/.home-backup-include.txt --delete-excluded
