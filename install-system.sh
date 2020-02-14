e3f6420c934d67de833e9a3bc6747e6f31685f05
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2020-02-13 23:30:07 -0500

    Add vim to pacstrap

--- install-system.sh
+++ install-system.sh
@@ -39,8 +39,8 @@ main(){
   mkdir /mnt/efi
   mount /dev/sda1 /mnt/efi
 
-  echo 'Install base system'
-  pacstrap /mnt base
+  echo 'Install base system + vim'
+  pacstrap /mnt base vim
 
   ########## CONFIGURING SYSTEM #############
   genfstab -U /mnt >> /mnt/etc/fstab
