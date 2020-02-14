d3fe2f36b0f287f1bc81776319baf75f2562441d
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2020-02-13 23:27:04 -0500

    Echo each step

--- install-system.sh
+++ install-system.sh
@@ -45,23 +45,30 @@ main(){
   ########## CONFIGURING SYSTEM #############
   genfstab -U /mnt >> /mnt/etc/fstab
 
+  echo 'Change Root'
   arch-chroot /mnt
 
   # Time
+  echo 'Set Time'
   ln -sf /usr/share/zoneinfo/America/Lima /etc/localtime
   hwclock --systohc
 
+  echo 'Generate locale'
   # Locatization
   echo "en_US.UTF-8 UTF-8  " >> /etc/locale.gen
   locale-gen
 
+  echo 'Set LANG'
   # Create the locale.conf(5) file, and set the LANG variable accordingly:
   echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
+  echo 'Persist Keyboard'
   # If you set the keyboard layout, make the changes persistent in vconsole.conf(5):
   echo "KEYMAP=us" >> /etc/vconsole.conf
 
+  echo 'Set hostname'
   echo "luis-pc" > /etc/hostname
+
   echo "127.0.0.1	localhost" >> /etc/hosts
   echo "::1		localhost" >> /etc/hosts
   echo "127.0.1.1	luis-pc.localdomain	luis-pc" >> /etc/hosts
@@ -70,6 +77,7 @@ main(){
 
   pacman -S grub efibootmgr
 
+  echo 'Install GRUB'
   grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
   grub-mkconfig -o /boot/grub/grub.cfg
 }
